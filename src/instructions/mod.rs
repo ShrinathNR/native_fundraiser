f8ff91c2dc126db04b385f595dbb1e8ec3934e9a
ShrinathNR <nr.shrinath@gmail.com>
2025-01-10 17:36:24 +0530

    add ix match

---
+++ src/instructions/mod.rs
@@ -0,0 +1,22 @@
+use pinocchio::program_error::ProgramError;
+
+pub enum FundraiserInstruction {
+    Initialize,
+    Contribute,
+    Checker,
+    Refund
+}
+
+impl TryFrom<&u8> for FundraiserInstruction {
+    type Error = ProgramError;
+
+    fn try_from(value: &u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(FundraiserInstruction::Initialize),
+            1 => Ok(FundraiserInstruction::Contribute),
+            2 => Ok(FundraiserInstruction::Checker),
+            3 => Ok(FundraiserInstruction::Refund),
+            _ => Err(ProgramError::InvalidInstructionData)
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,27 @@
-use pinocchio::pubkey::Pubkey;
+use instructions::FundraiserInstruction;
+use pinocchio::{account_info::AccountInfo, entrypoint, program_error::ProgramError, pubkey::Pubkey, ProgramResult};
 
 pub mod state;
+pub mod instructions;
 
 pub const ID: Pubkey = five8_const::decode_32_const("ADcaide4vBtKuyZQqdU689YqEGZMCmS4tL35bdTv9wJa");
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let (discriminator, data) = instruction_data
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    match FundraiserInstruction::try_from(discriminator)? {
+        FundraiserInstruction::Initialize => todo!(),
+        FundraiserInstruction::Contribute => todo!(),
+        FundraiserInstruction::Checker => todo!(),
+        FundraiserInstruction::Refund => todo!(),
+    }
+
+}
